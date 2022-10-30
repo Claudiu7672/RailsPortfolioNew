dde377b81ae0720572d381530ca6f45cd9c361cc
Claudiu Florin Popa <claudiup_15@yahoo.com>
2022-10-30 18:02:40 +0100

    Implemented logout functionality and dynamic view rendering for auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+  end
+
   # Devise routes
   devise_for :users, path: "", path_names: { sign_in: "login", sign_out: "logout", sign_up: "register" }
 
