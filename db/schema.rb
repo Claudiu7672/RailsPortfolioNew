d406918ecac37e4e33f174664e5d9c5d88e0c08b
Claudiu Florin Popa <claudiup_15@yahoo.com>
2022-10-31 12:47:14 +0100

    Migrated the DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_31_113044) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_31_114200) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "portfolios", force: :cascade do |t|
+    t.string "title"
+    t.string "subtitle"
+    t.text "body"
+    t.string "main_image"
+    t.string "thumb_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
